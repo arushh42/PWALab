02ee82767e69283164f1401c4dc0a2a55341578b
Arush Karekar <117122136+arushh42@users.noreply.github.com>
2024-04-30 09:42:13 +0530

    Update sw1.js

--- sw1.js
+++ sw1.js
@@ -1,32 +1,29 @@
 //after refresh
-var cacheurl = ["/assets/css/style.css", 
-"/index.html",
-"/assets/images/courses-1.jpg",
-"/assets/images/service-1.svg",
+var cacheurl = [
+"/Users/arushkarekar/Desktop/MADLAB/LabPWA/PWALab/index.html",
+"/Users/arushkarekar/Desktop/MADLAB/LabPWA/PWALab/assets/images/courses-1.jpg",
+"/Users/arushkarekar/Desktop/MADLAB/LabPWA/PWALab/assets/images/courses-5.jpg",
 "/assets/images/about-banner.jpg",
 "/assets/images/title-shape.png"
 ]
 
-self.addEventListener('install',
-    event=>{
+
+self.addEventListener('install', event => {
         event.waitUntil(
-            caches.open('Mycachefile')
+            caches.open('MyCacheFile')
             .then(cache=>{
                 return cache.addAll(cacheurl);
             })
-        )
-     })
-
-self.addEventListener('fetch',
-    event => {
-        event.respondWith(
-            caches.match(event.request)
-            .then(response=>{
-                if (response){
-                    return response;
-                }
-            return fetch(event.response);
-            })
-        )
+        );
     }
     )
+
+self.addEventListener('fetch', event=>{
+        event.respondWith(caches.match(event.request))
+        .then(res => {
+            if (res){
+            return res;
+            }
+            return fetch(event.request); 
+        })
+    })
